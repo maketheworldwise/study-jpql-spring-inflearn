a738f196d73e03c5c8b4516678885bdda8dd460d
Kevin Ahn <maketheworldwise@gmail.com>
2023-01-30 18:29:42 +0900

    Feature: 반환 타입
    - TypedQuery
    - Query

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -4,6 +4,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import jpql.domain.Member;
 
@@ -21,6 +23,13 @@ public class JpaMain {
 			member.setName("member1");
 
 			em.persist(member);
+
+			// 반환 타입 명확할 때
+			TypedQuery<Member> typedQuery1 = em.createQuery("SELECT m FROM Member m", Member.class);
+			TypedQuery<String> typedQuery2 = em.createQuery("SELECT m.name FROM Member m", String.class);
+
+			// 반환 타입 불명확할 때
+			Query query = em.createQuery("SELECT m.name FROM Member m");
 
 			tx.commit();
 		} catch (Exception e) {
